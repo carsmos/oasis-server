ca5ef62ee2fd1acf67bd4d56c15149741ca57d5c
jsc <jsc181200@163.com>
2022-01-13 14:53:19 +0800

    modified scenario interface field

--- sdgApp/Application/ScenariosFacadeService/AssembleService.py
+++ sdgApp/Application/ScenariosFacadeService/AssembleService.py
@@ -7,12 +7,14 @@ def AssembleScenarioService(scenario_create_model: dict, db):
     scenario_dto = scenario_create_model
     dynamic_scene = DynamicSceneQueryUsercase(
         db_session=db).find_specified_scenario(scenario_dto["dynamic_scene_id"])
-    environment = ""
-    if scenario_dto["env_id"]:
+    environments = ["ClearNoon", "CloudyNoon", "WetNoon", "WetCloudyNoon", "SoftRainNoon",
+                    "MidRainyNoon", "HardRainNoon", "ClearSunset", "CloudySunset", "WetSunset",
+                    "WetCloudySunset", "SoftRainSunset", "MidRainSunset", "HardRainSunset"]
+    if scenario_dto["env_id"] in environments:
+        environment = scenario_dto["env_id"]
+    else:
         environment = EnvQueryUsercase(
             db_session=db).find_specified_env(scenario_dto["env_id"])
-    if scenario_dto["env_name"]:
-        environment = scenario_dto["env_name"]
     scenario_dto.update({"scenario_param": {}})
     scenario_dto["scenario_param"]["map_name"] = scenario_dto["map_name"]
     scenario_dto["scenario_param"]["dynamic_scene"] = dynamic_scene
